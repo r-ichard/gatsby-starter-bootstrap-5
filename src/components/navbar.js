d847ab745e7212f63e7f0b66bc14ded09d06f5f0
r-ichard <richard.raduly@conity.fr>
2022-01-03 18:57:36 -0500

    Upgraded to gatsby 4 and latest bootstrap

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,7 +1,6 @@
 import * as React from "react"
 import PropTypes from "prop-types"
 import { Link } from "gatsby"
-import { useEffect } from "react"
 
 const isActive = ({ isCurrent }) => {
   return isCurrent ? { className: "nav-link active" } : {className: "nav-link"}
